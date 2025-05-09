facc7d197d653a1be8f4eff78e5771835d02bf42
Nav2-002 <khiemdo2009@gmail.com>
2025-05-09 18:58:08 +0700

    create

--- CHUOI_COBAN/CHUOI_COBAN.cpp
+++ CHUOI_COBAN/CHUOI_COBAN.cpp
@@ -1,6 +1,8 @@
 #include <cmath>
 #include <climits>
 #include <iostream>
+#include <cstring>
+#include <cctype>
 using namespace std;
 
 int DoDai(char str[1000]) {
@@ -137,11 +139,69 @@ void VietTat(char str[100]) {
         }
     }
 }
+
+void TangQuaPetya(char a[101],char b[101]) {
+	int sum=0;
+	
+	for (int i=0;i<strlen(a);i++) {
+	a[i]=(tolower(a[i]));
+	b[i]=(tolower(b[i]));
+	}
+	
+	int compare = strcmp(a,b);
+		if (compare<0) {
+			sum=-1;
+		}
+		else if (compare>0) {
+			sum=1;
+		}
+	cout<<sum;
+}
+
+void PetyaDaBong(char a[10000]) {
+	int count0=0;
+	int count1=0;
+	bool check = false;
+	
+	for (int i=0;i<strlen(a);i++) {
+		//xet 0
+		if (a[i] == '0') {
+			count0++;
+	 		if (count0<7) {
+	 			if (a[i+1] == '1') {
+	 				count0=0;
+	 			}
+	 		}
+		}
+		//xet 1
+		if (a[i] == '1') {
+			count1++;
+	 		if (count1<7) {
+	 			if (a[i+1] == '0') {
+	 				count1=0;
+	 			}
+	 		}
+		}
+		if (count0==7||count1==7) {
+		check = true;
+		}
+	}
+	 
+	//In YES/NO
+	if (check == true) {
+		cout<<"YES";
+	}
+	else {
+		cout<<"NO";
+	}
+}
+
 int main()
 {   
-    char str[1000];
+    char str[100];
     //cin.ignore(1) bo qua 1 buoc
-    cin.getline(str, 1000);
-    VietTat(str);
+	char str2[100];
+    cin>>str;
+    PetyaDaBong(str);
     return 0;
 }
