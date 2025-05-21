7b36d7c85c702c69c392cb0701c92e5590a815a0
Nav2-002 <khiemdo2009@gmail.com>
2025-05-21 22:43:06 +0700

    create

--- CAUTRUC_COBAN/CAUTRUC_BAI8/CAUTRUC_BAI8.cpp
+++ CAUTRUC_COBAN/CAUTRUC_BAI8/CAUTRUC_BAI8.cpp
@@ -29,6 +29,56 @@ int TongGiaTienCacVe(ve a[10],int n) {
 	return sum;
 }
 
+void SapXepTangDanXuatChieu(ve a[10], int n) {
+	char A[11],B[11], tam[31],tam1[31];
+	char A1[11] = "00/00/0000";
+	char B1[11] = "00/00/0000";
+	char A2[9],B2[9];
+	for (int i=0;i<n;i++) {
+		for (int j=0;j<n-1;j++) {
+			int k = 0;
+			for (int e=0;e<11;e++) {
+				A[k] = a[j]->ngay[e];
+				B[k] = a[j+1]->ngay[e];
+				if (e>=0 && e<=8) {
+					A2[k] = a[j]->thoigian[e];
+					B2[k] = a[j+1]->thoigian[e];
+				}
+				if (e>=6) {
+					A1[k] = a[j]->ngay[e];
+					B1[k] = a[j+1]->ngay[e];
+				}
+				k++;
+			}
+			if (strcmp(A1,B1)==0) {
+				if (strcmp(A,B)>0) {
+				strcpy(tam,a[j]->ten);
+				strcpy(tam1,a[j]->hsd);
+				
+				strcpy(a[j]->ten,a[j+1]->ten);
+				strcpy(a[j]->hsd,a[j+1]->hsd);
+				
+				strcpy(a[j+1]->ten,tam);
+				strcpy(a[j+1]->hsd,tam1);
+				}
+			}
+			else if (strcmp(A1,B1)>0) {
+				strcpy(tam,a[j]->ten);
+				strcpy(tam1,a[j]->hsd);
+				
+				strcpy(a[j]->ten,a[j+1]->ten);
+				strcpy(a[j]->hsd,a[j+1]->hsd);
+				
+				strcpy(a[j+1]->ten,tam);
+				strcpy(a[j+1]->hsd,tam1);
+				}
+			}	
+		}
+	cout<<"Hop sua tang dan HSD la: ";
+	for (int i=0;i<n;i++) {
+		cout<<a[i]->ten<<" ";
+	}
+}
 int main() {
 	return 0;
 }
