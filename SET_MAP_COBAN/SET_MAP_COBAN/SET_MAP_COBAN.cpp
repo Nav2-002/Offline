aed97cf97a3af67c0019ffcb70fcc2a536c0a17f
NMTThinh <khiemdo2009@gmail.com>
2025-06-07 10:48:38 +0700

    a

---
+++ SET_MAP_COBAN/SET_MAP_COBAN/SET_MAP_COBAN.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <set>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+int DemSoPhanTuKhacNhauTrongSet(vector<int>v,set<int>s) {
+    int sum = 0;
+    for (auto item : v) {
+        s.insert(item);
+    }
+    for (auto item : s) {
+        sum++;
+    }
+    return sum;
+}
+void TruyVanTrongSet(vector<int> v, int tv) {
+    set<int>s;
+    for (auto item : v) s.insert(item);
+    if (s.find(tv) == s.end()) cout << "NO";
+    else cout << "YES";
+}
+
+void Lap(int L,vector<int>v) {
+    set<int>s; 
+    for (auto item : v) s.insert(item);
+    int tv;
+    for (int i = 0; i < L; i++) {
+        cin >> tv;
+        TruyVanTrongSet(v, tv);
+    }
+}
+
+void LietKePhanTuKhacNhau(vector<int> v) {
+    set<int> s;
+    for (auto item : v) {
+        if (s.count(item) == 0) {
+            cout << item;
+            s.insert(item);
+        }
+    }
+}
+
+void LietKePhanTuKhacNhauNhoDenLon(vector<int>v) {
+    set<int>s;
+    for (auto item : v) {
+        s.insert(item);
+    }
+    for (auto item : s) {
+        cout << item;
+    }
+}
+
+void DemSoPhanTuKhacNhauTrongMap(vector <int>v) {
+    map <int, int> m;
+    for (auto item : v) {
+        m[item] = 1;
+    }
+    cout << m.size();
+}
+
+void DemLanXuatHien(vector <int>v) {
+    map <int, int> m;
+    for (auto item : v) {
+        m[item]++;
+    }
+    for (auto item : m) {
+        cout << item.first << "="<< item.second << " ";
+    }
+}
+
+int main()
+{
+    vector<int> v = { 9,2,3,6,7,8,8,8 };
+    DemLanXuatHien(v);
+
+    return 0;
+}
+
